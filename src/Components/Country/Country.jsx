82c4a875c9df4667b2e939374134a77b5384b28d
iftakhersifat <hossensifat615@gmail.com>
2025-03-27 22:09:55 +0600

    added independent country check

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -6,7 +6,8 @@ const Country = ({ country }) => {
         <div>
             <h1>{country.name.common}</h1>
             <img src={country.flags.png} alt="" />
-            <h3>Capital: {country.capital}</h3>
+            <h2>Capital: {country.capital}</h2>
+            <h3>{country.independent ? "Yes" : "Not"} Independent Country</h3>
             
         </div>
     );
