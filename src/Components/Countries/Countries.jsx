ea531c82817e74e7c429cecee7b9ef9da14932d5
iftakhersifat <hossensifat615@gmail.com>
2025-03-27 22:13:13 +0600

    added key

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -8,7 +8,7 @@ const Countries = ({countriesPromise}) => {
         <div>
             <h1>Country List : {countries.length}</h1>
             {
-                countries.map(country=> <Country country={country}></Country>)
+                countries.map(country=> <Country key={country.cca2} country={country}></Country>)
             }
         </div>
     );
