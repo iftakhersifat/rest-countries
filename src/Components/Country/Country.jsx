cc48333c79999d230f5b76958c89046be2b7675f
iftakhersifat <hossensifat615@gmail.com>
2025-03-27 22:15:33 +0600

    create a visit button

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 const Country = ({ country }) => {
     console.log(country);
+
+    const handelVisitButton =()=>{
+        alert("clicked")
+    }
+
     return (
         <div>
             <h1>{country.name.common}</h1>
@@ -10,6 +15,7 @@ const Country = ({ country }) => {
             <h3>{country.independent ? "Yes" : "Not"} Independent Country</h3>
             <h4>Population: {country.population}</h4>
             <h4>Region: {country.region}</h4>
+            <button onClick={handelVisitButton}>Not Visit</button>
         </div>
     );
 };
